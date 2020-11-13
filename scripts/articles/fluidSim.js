65113b0a02a2ffd55a1e40e6082a0dbab6143213
Craig Balfour <Craig.Balfour@sit.ac.nz>
2020-11-13 16:56:00 +1300

    a little work on fluid sim. Still broken af

--- scripts/articles/fluidSim.js
+++ scripts/articles/fluidSim.js
@@ -10,6 +10,14 @@ let dotSize = 0;
 let gridSize = 100;
 let grid;
 
+function makeBlankGrid(){
+	const g = [];
+	for(let x = 0; x < gridSize; x++){
+		g[x] = [];
+	}
+	return g;
+}
+
 function setUp(){
 	c = document.getElementById('canv');
 	w = document.getElementById('sizeCalc').getBoundingClientRect().width;
@@ -27,19 +35,21 @@ function setUp(){
 		for(let y = 0; y < gridSize; y++){
 			grid[x][y] = {
 				v: {
-					x: Math.sin(1 * Math.PI * 2 * mapLinear(y, 0, gridSize, -1, 1)),
-					y: Math.cos(1 * Math.PI * 2 * mapLinear(x, 0, gridSize, -1, 1))
+					x: Math.sin(Math.PI * 2 * mapLinear(y, 0, gridSize, -1, 1)),
+					y: Math.cos(Math.PI * 2 * mapLinear(x, 0, gridSize, -1, 1))
 				},
-				d: Math.sin(1 * Math.PI * 2 * mapLinear(y, 0, gridSize, -1, 1)) * Math.cos(1 * Math.PI * 2 * mapLinear(x, 0, gridSize, -1, 1)),
+				d: Math.random(),// Math.sin(1 * Math.PI * 2 * mapLinear(y, 0, gridSize, -1, 1)) * Math.cos(1 * Math.PI * 2 * mapLinear(x, 0, gridSize, -1, 1)),
 				x: mapLinear(x, 0, gridSize - 1, padding, w - padding),
-				y: mapLinear(y, 0, gridSize - 1, padding, h - padding)
+				y: mapLinear(y, 0, gridSize - 1, padding, h - padding),
+				color: ''//'hsl(' + vecToAngleDeg(grid[x][y].v) + ',100%,' + (grid[x][y].d * 100) + '%)'
 			};
+			grid[x][y].color = vecToAngleDeg(grid[x][y].v);
 		}
 	}
 	setInterval(() => {
 		updateGrid();
 		draw();
-	},50);
+	},100);
 
 }
 
@@ -57,17 +67,66 @@ function vecMag(vec){
 }
 
 function updateGrid(){
+	let newGrid = makeBlankGrid();
 	for(let x = 0; x < gridSize; x++){
 		for(let y = 0; y < gridSize; y++){
 			const cell = grid[x][y];
+			const newCell = {
+				x: cell.x,
+				y: cell.y,
+				color: cell.color
+			};
+			
 			const vi = invertVelocity(cell.v);
-			const oldX = x + vi.x;
-			const oldY = y + vi.y;
+			const oldX = x - vi.x;
+			const oldY = y - vi.y;
+			
+			if(oldX <= 0 || oldX >= gridSize-1 || oldY <= 0 || oldY >= gridSize-1){
+				newCell.v = vi;
+			}
+			else{
+				const xLow = ~~oldX;
+				const xHigh = xLow + 1;
+				const xFrac = oldX - xLow;
 
-			const d = grid[clamp(round(oldX), 0, gridSize - 1)][clamp(round(oldY), 0, gridSize - 1)].d; //this is shit! Use linear interpolate
-			cell.d = d;
+				const yLow = ~~oldY;
+				const yHigh = yLow + 1;
+				const yFrac = oldY - yLow;
+
+				const cornerA = grid[xLow][yLow];
+				const cornerB = grid[xLow][yHigh];
+				const cornerC = grid[xHigh][yLow];
+				const cornerD = grid[xHigh][yHigh];
+				
+				const topRailX = cornerB.v.x * xFrac + cornerA.v.x * (1- xFrac);
+				const bottomRailX = cornerD.v.x * xFrac + cornerC.v.x * (1- xFrac);
+				const midRailX = topRailX * yFrac + bottomRailX * (1- yFrac);
+				
+				const topRailY = cornerB.v.y * xFrac + cornerA.v.y * (1- xFrac);
+				const bottomRailY = cornerD.v.y * xFrac + cornerC.v.y * (1- xFrac);
+				const midRailY = topRailY * yFrac + bottomRailY * (1- yFrac);
+				
+				const topRailColor = cornerB.color * xFrac + cornerA.color * (1- xFrac);
+				const bottomRailColor = cornerD.color * xFrac + cornerC.color * (1- xFrac);
+				const midRailColor = topRailColor * yFrac + bottomRailColor * (1- yFrac);
+
+				newCell.v = {x: midRailX, y: midRailY};
+				newCell.color = midRailColor;
+			}
+			newGrid[x][y] = newCell;
+			
+			//if(oldX < 0 || oldX >= gridSize || oldY < 0 || oldY >= gridSize){
+				//cell.v = vi;
+			//}
+			//else{
+				//const d = grid[clamp(round(oldX), 0, gridSize - 1)][clamp(round(oldY), 0, gridSize - 1)].d; //this is shit! Use linear interpolate
+				////const v = grid[clamp(round(oldX), 0, gridSize - 1)][clamp(round(oldY), 0, gridSize - 1)].v; //this is shit! Use linear interpolate
+				//cell.d = d;
+				////cell.v = {x: (v.x + cell.v.x)/2, y: (v.y + cell.v.y)/2};
+			//}
 		}
 	}
+	grid = newGrid;
 }
 
 function draw(){
@@ -77,7 +136,8 @@ function draw(){
 	ctx.fillStyle = 'red';
 	for(let x = 0; x < gridSize; x++){
 		for(let y = 0; y < gridSize; y++){
-			ctx.fillStyle = 'hsl(' + vecToAngleDeg(grid[x][y].v) + ',100%,' + (grid[x][y].d * 100) + '%)';
+			//ctx.fillStyle = 'hsl(' + vecToAngleDeg(grid[x][y].v) + ',100%,' + (grid[x][y].d * 100) + '%)';
+			ctx.fillStyle = 'hsl(' + grid[x][y].color + ',100%,50%)';
 			ctx.fillRect(grid[x][y].x - dotSize/2, grid[x][y].y - dotSize/2, dotSize,dotSize);
 		}
 	}
